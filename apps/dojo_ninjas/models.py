87871010bb8c3559b7449abde5b2702d7ccf666a
ehoversten <ehoversten@gmail.com>
2018-05-16 15:13:39 -0400

    initial commit

---
+++ apps/dojo_ninjas/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Dojo(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    desc = models.CharField(max_length=255)
+
+class Ninja(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    dojo = models.ForeignKey(Dojo, related_name="ninjas", on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
